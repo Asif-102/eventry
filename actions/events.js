9565f8f56317ef56db0ff7b791a6d658afeddc9f
Asif-102 <asif1zero2@gmail.com>
2024-12-17 03:23:01 +0600

    removed metadata using lean and convert _id to id

--- actions/events.js
+++ actions/events.js
@@ -1,13 +1,14 @@
 import dbConnect from "@/dbConnect/connectMongo";
 import { eventModel } from "@/models/event-models";
+import { replaceMongoIdInArray } from "@/utils/data-util";
 
 export const getAllEvents = async () => {
   try {
     await dbConnect();
 
-    const allEvents = await eventModel.find();
+    const allEvents = await eventModel.find().lean();
 
-    return allEvents;
+    return replaceMongoIdInArray(allEvents);
   } catch (err) {
     console.log(err);
   }
