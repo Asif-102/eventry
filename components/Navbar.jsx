23ddcad5f016f7b7066dcf1b6ac980e008dd135c
Asif-102 <asif1zero2@gmail.com>
2024-12-17 00:39:29 +0600

    event details route created done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav>
       <div className="container flex justify-between items-center py-4">
         <div className="nav-brand">
-          <a href="index.html">
+          <Link href="/">
             <Image src="/logo.svg" alt="Eventry" width={135} height={135} />
-          </a>
+          </Link>
         </div>
 
         <ul className="flex gap-4 text-[#9C9C9C]">
