8e6d42e6d5497c00190815ff6cce41fd958e65a2
Asif-102 <asif1zero2@gmail.com>
2024-12-18 02:33:44 +0600

    test email sent successfully

--- app/actions/index.js
+++ app/actions/index.js
@@ -52,7 +52,7 @@ async function addGoingEvent(eventId, user) {
 
 async function sendEmail(eventId, user) {
   try {
-    console.log(eventId, user, process.env.RESEND_API_KEY);
+    // console.log(eventId, user, process.env.RESEND_API_KEY);
     const event = await getEventById(eventId);
     const resend = new Resend(process.env.RESEND_API_KEY);
     const message = `Dear ${user?.name}, you have been successfully registered for the event, ${event?.name}. Please carry this email and your official id to the venue. We are excited to have you here.`;
@@ -62,6 +62,7 @@ async function sendEmail(eventId, user) {
       subject: "Successfully Registered for the event!",
       react: EmailTemplate({ message }),
     });
+    console.log("🚀 ~ sendEmail ~ sent:", sent);
   } catch (error) {
     throw error;
   }
